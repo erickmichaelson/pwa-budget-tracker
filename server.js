9d1bb375da1b0c976eec419bad056ff5bd377180
Erick Michaelson <emich09@gmail.com>
2021-07-01 12:40:03 -0400

    deploying

--- server.js
+++ server.js
@@ -15,11 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 // routes
 app.use(require("./routes/api.js"));
